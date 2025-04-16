297417949f51af0a54693264420df4f6920e5bc0
DilmyPerera <samadiniperera20@gmail.com>
2025-04-16 16:58:17 +0530

    Create Spring Starter Project

---
+++ Backend/MahindaPlants/src/main/java/lk/ac/vau/fas/MahindaPlantsApplication.java
@@ -0,0 +1,13 @@
+package lk.ac.vau.fas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahindaPlantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahindaPlantsApplication.class, args);
+	}
+
+}
