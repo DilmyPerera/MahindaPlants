5e5c776c8865bec790a4ff6603b1074dc9aeaf68
DilmyPerera <samadiniperera20@gmail.com>
2025-04-18 00:40:27 +0530

    Add JWT-based authentication and security configuration
    - Implemented `AuthUser` model to represent authenticated users.
    - Added `CorsConfig` to handle Cross-Origin Resource Sharing settings.
    - Created `CustomUserDetailsService` to load user-specific data.
    - Introduced `JwtAuthFilter` to intercept and validate JWT tokens.
    - Added utility methods for token creation and validation in `JwtUtils`.
    - Configured Spring Security in `SecurityConfig` to use JWT-based auth.

---
+++ Backend/MahindaPlants/src/main/java/lk/ac/vau/fas/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package lk.ac.vau.fas.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lk.ac.vau.fas.entity.User;
+import lk.ac.vau.fas.exception.NotFoundException;
+import lk.ac.vau.fas.repository.UserRepo;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepo userRepo;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepo.findByEmail(username)
+                .orElseThrow(()-> new NotFoundException("User/ Email Not found"));
+
+        return AuthUser.builder()
+                .user(user)
+                .build();
+    }
+}
+ 
