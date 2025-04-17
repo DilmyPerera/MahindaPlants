7ce5f669f970a99d0008a1d6bd6818a29240b41f
DilmyPerera <samadiniperera20@gmail.com>
2025-04-17 22:13:34 +0530

    Create InvalidCredentialException.java

---
+++ Backend/MahindaPlants/src/main/java/lk/ac/vau/fas/exception/InvalidCredentialException.java
@@ -0,0 +1,8 @@
+package lk.ac.vau.fas.exception;
+
+public class InvalidCredentialException extends RuntimeException{
+
+	public InvalidCredentialException(String message) {
+		super(message);
+	}
+}
