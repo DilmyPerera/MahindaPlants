e0cf110e78a16014ca1be69531f62c0d831fee0c
DilmyPerera <samadiniperera20@gmail.com>
2025-04-18 00:40:34 +0530

    Update UserRepo.java

--- Backend/MahindaPlants/src/main/java/lk/ac/vau/fas/repository/UserRepo.java
+++ Backend/MahindaPlants/src/main/java/lk/ac/vau/fas/repository/UserRepo.java
@@ -1,10 +1,12 @@
 package lk.ac.vau.fas.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import lk.ac.vau.fas.entity.User;
 
 public interface UserRepo extends JpaRepository<User, Long>{
 
-	//optional<User> findByEmail(String email);
+	Optional<User> findByEmail(String email);
 }
