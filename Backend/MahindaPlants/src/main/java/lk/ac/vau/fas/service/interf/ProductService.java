216be0532db361e5d12765e756ce6b8c7cb0c35f
DilmyPerera <samadiniperera20@gmail.com>
2025-04-25 22:45:04 +0530

    Add `ProductService` interface with product CRUD and search methods

---
+++ Backend/MahindaPlants/src/main/java/lk/ac/vau/fas/service/interf/ProductService.java
@@ -0,0 +1,19 @@
+package lk.ac.vau.fas.service.interf;
+
+import java.math.BigDecimal;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lk.ac.vau.fas.dto.Response;
+
+public interface ProductService {
+
+	Response createProduct(Long categoryId, MultipartFile image, String name, String description, BigDecimal price);
+    Response updateProduct(Long productId, Long categoryId, MultipartFile image, String name, String description, BigDecimal price);
+    Response deleteProduct(Long productId);
+    Response getProductById(Long productId);
+    Response getAllProducts();
+    Response getProductsByCategory(Long categoryId);
+    Response searchProduct(String searchValue);
+}
+ 
