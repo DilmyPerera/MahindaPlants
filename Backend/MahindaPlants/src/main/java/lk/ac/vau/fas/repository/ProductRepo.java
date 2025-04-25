d4af4dd1349de4cc09c592f7748d3897f6eda2e9
DilmyPerera <samadiniperera20@gmail.com>
2025-04-25 22:44:32 +0530

    Update ProductRepo.java

--- Backend/MahindaPlants/src/main/java/lk/ac/vau/fas/repository/ProductRepo.java
+++ Backend/MahindaPlants/src/main/java/lk/ac/vau/fas/repository/ProductRepo.java
@@ -2,12 +2,14 @@ package lk.ac.vau.fas.repository;
 
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import lk.ac.vau.fas.entity.Product;
 
 public interface ProductRepo extends JpaRepository<Product, Long>{
 
-//	List<Product> findByCategoryId(Long CategoryId):
-//	List<Product> findByNameContainingOrDescriptionContaining(String name, String description);
+	List<Product> findByCategoryId(Long CategoryId);
+	List<Product> findByNameContainingOrDescriptionContaining(String name, String description);
 }
