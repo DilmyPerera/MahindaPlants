5b9cde09f25b034bfe413db29a36beaf43c97015
DilmyPerera <samadiniperera20@gmail.com>
2025-04-25 14:30:29 +0530

    Add `CategoryService` interface with CRUD method definitions

---
+++ Backend/MahindaPlants/src/main/java/lk/ac/vau/fas/service/interf/CategoryService.java
@@ -0,0 +1,12 @@
+package lk.ac.vau.fas.service.interf;
+
+import lk.ac.vau.fas.dto.CategoryDto;
+import lk.ac.vau.fas.dto.Response;
+
+public interface CategoryService {
+	Response createCategory(CategoryDto categoryRequest);
+    Response updateCategory(Long categoryId, CategoryDto categoryRequest);
+    Response getAllCategories();
+    Response getCategoryById(Long categoryId);
+    Response deleteCategory(Long categoryId);
+}
