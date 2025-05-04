784b1f69f4ec0a20024b40b4316e270de90db519
DilmyPerera <samadiniperera20@gmail.com>
2025-05-04 22:48:05 +0530

    Add `AddressController` with endpoint to save or update address

---
+++ Backend/MahindaPlants/src/main/java/lk/ac/vau/fas/controller/AddressController.java
@@ -0,0 +1,26 @@
+package lk.ac.vau.fas.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lk.ac.vau.fas.dto.AddressDto;
+import lk.ac.vau.fas.dto.Response;
+import lk.ac.vau.fas.service.interf.AddressService;
+
+@RestController
+@RequestMapping("/address")
+@RequiredArgsConstructor
+public class AddressController {
+
+    private final AddressService addressService;
+
+    @PostMapping("/save")
+    public ResponseEntity<Response> saveAndUpdateAddress(@RequestBody AddressDto addressDto){
+        return ResponseEntity.ok(addressService.saveAndUpdateAddress(addressDto));
+    }
+}
+
