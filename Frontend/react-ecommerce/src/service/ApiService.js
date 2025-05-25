fe2a7580e4f6751aed5afb7672421529eb475277
DilmyPerera <samadiniperera20@gmail.com>
2025-05-25 11:30:02 +0530

    Refactor ApiService to include user authentication methods for registration, login, and fetching user info

--- Frontend/react-ecommerce/src/service/ApiService.js
+++ Frontend/react-ecommerce/src/service/ApiService.js
@@ -11,4 +11,25 @@ export default class ApiService {
             "Content-Type": "application/json"
         };
     }
+
+    // Auth API , USER API
+
+    static async registerUser(registration) {
+        const response = await axios.post(`${this.BASE_URL}/auth/register`, registration)
+        return response.data;
+    }
+
+
+    static async loginUser(loginDetails) {
+        const response = await axios.post(`${this.BASE_URL}/auth/login`, loginDetails)
+        return response.data;
+    }
+
+
+    static async getLoggedInUserInfo() {
+        const response = await axios.get(`${this.BASE_URL}/user/my-info`, {
+            headers: this.getHeader()
+        });
+        return response.data;
+    }
 }
