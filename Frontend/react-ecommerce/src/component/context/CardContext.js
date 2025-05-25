d6921c87112dd4cd712a881efb6159b8df14fdd5
DilmyPerera <samadiniperera20@gmail.com>
2025-05-25 20:33:06 +0530

    Add initial state and context setup for CartContext

--- Frontend/react-ecommerce/src/component/context/CardContext.js
+++ Frontend/react-ecommerce/src/component/context/CardContext.js
@@ -1,1 +1,8 @@
 import React, { createContext, useReducer, useContext, useEffect } from "react";
+
+const CartContext = createContext();
+
+const initialState = {
+    cart: JSON.parse(localStorage.getItem('cart')) || [],
+}
+
