50b22203166aa47f477e60b7e80ef1f9f9f5c323
DilmyPerera <samadiniperera20@gmail.com>
2025-05-28 16:28:15 +0530

    Implement routing for Home component in App.js

--- Frontend/react-ecommerce/src/App.js
+++ Frontend/react-ecommerce/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import { CartProvider } from './component/context/CardContext';
 import Navbar from './component/common/Navbar';
 import Footer from './component/common/footer';
+import Home from './component/pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <CartProvider>
         <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
         <Footer />
       </CartProvider>
     </BrowserRouter>
