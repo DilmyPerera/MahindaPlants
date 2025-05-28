f41b7c2fee86ae50847ccdcbf9ca30364f18acbe
DilmyPerera <samadiniperera20@gmail.com>
2025-05-28 15:30:26 +0530

    Add Footer component to App for improved layout

--- Frontend/react-ecommerce/src/App.js
+++ Frontend/react-ecommerce/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './component/context/CardContext';
 import Navbar from './component/common/Navbar';
+import Footer from './component/common/footer';
 
 function App() {
   return (
     <BrowserRouter>
       <CartProvider>
         <Navbar />
+        <Footer />
       </CartProvider>
     </BrowserRouter>
   );
