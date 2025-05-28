9d76653bee3cf73bcd27daf5f1ed57e5c752c4a5
DilmyPerera <samadiniperera20@gmail.com>
2025-05-28 16:16:09 +0530

    Add imports for React, routing, ProductList, Pagination, and ApiService in Home component

---
+++ Frontend/react-ecommerce/src/component/pages/Home.jsx
@@ -0,0 +1,5 @@
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import ProductList from "../common/ProductList";
+import Pagination from "../common/Pagination";
+import ApiService from "../../service/ApiService";
