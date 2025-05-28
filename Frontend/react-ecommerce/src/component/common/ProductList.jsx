3d7fcac2ef491daa4c19af10126a392dce842774
DilmyPerera <samadiniperera20@gmail.com>
2025-05-28 15:42:23 +0530

    Add ProductList component for managing cart items

---
+++ Frontend/react-ecommerce/src/component/common/ProductList.jsx
@@ -0,0 +1,21 @@
+
+const ProductList = ({ products }) => {
+    const { cart, dispatch } = useCart();
+
+    const addToCart = (product) => {
+        dispatch({ type: 'ADD_ITEM', payload: product });
+    }
+
+    const incrementItem = (product) => {
+        dispatch({ type: 'INCREMENT_ITEM', payload: product });
+    }
+
+    const decrementItem = (product) => {
+
+        const cartItem = cart.find(item => item.id === product.id);
+        if (cartItem && cartItem.quantity > 1) {
+            dispatch({ type: 'DECREMENT_ITEM', payload: product });
+        } else {
+            dispatch({ type: 'REMOVE_ITEM', payload: product });
+        }
+    }
