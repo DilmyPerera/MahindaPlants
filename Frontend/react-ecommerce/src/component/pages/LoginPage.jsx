e0f094889306b41c8597f25867a969a24674d61e
DilmyPerera <samadiniperera20@gmail.com>
2025-06-07 18:59:32 +0530

    Refactor LoginPage component to enhance form structure and user feedback display

--- Frontend/react-ecommerce/src/component/pages/LoginPage.jsx
+++ Frontend/react-ecommerce/src/component/pages/LoginPage.jsx
@@ -33,4 +33,34 @@ const LoginPage = () => {
             setMessage(error.response?.data.message || error.message || "unable to Login a user");
         }
     }
+
+    return (
+        <div className="register-page">
+            <h2>Login</h2>
+            {message && <p className="message">{message}</p>}
+            <form onSubmit={handleSubmit}>
+                <label>Email: </label>
+                <input
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    required />
+
+                <label>Password: </label>
+                <input
+                    type="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    required />
+
+                <button type="submit">Login</button>
+
+                <p className="register-link">
+                    Don't have an account? <a href="/register">Register</a>
+                </p>
+            </form>
+        </div>
+    )
 }
