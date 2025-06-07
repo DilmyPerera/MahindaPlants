46931f9ab51a48c1427a9ece5b5c3980cb2fabae
DilmyPerera <samadiniperera20@gmail.com>
2025-06-07 13:11:10 +0530

    Add product details fetching logic to ProductDetailsPage component

--- Frontend/react-ecommerce/src/component/pages/ProductDetailsPage.jsx
+++ Frontend/react-ecommerce/src/component/pages/ProductDetailsPage.jsx
@@ -1,3 +1,11 @@
-const ProductDetailsPage = () => {
+import { useEffect } from "react";
 
+const ProductDetailsPage = () => {
+    const { productId } = useParams();
+    const { cart, dispatch } = useCart();
+    const [product, setProduct] = useState(null);
+
+    useEffect(() => {
+        fetchProduct();
+    }, [productId])
 }
