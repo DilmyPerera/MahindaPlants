47f8e36d8012d0832c0d52701aa9f97b753d2eeb
DilmyPerera <samadiniperera20@gmail.com>
2025-06-07 17:03:57 +0530

    Add CategoryListPage component with category fetching logic

---
+++ Frontend/react-ecommerce/src/component/pages/CategoryListPage.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const CategoryListPage = () => {
+    const [categories, setCategories] = useState([]);
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        fetchCategories();
+    }, []);
+
+    const fetchCategories = async () => {
+        try {
+            const response = await ApiService.getAllCategory();
+            setCategories(response.categoryList || [])
+
+        } catch (err) {
+
+            setError(err.response?.data?.message || err.message || 'Unable to fetch categories')
+
+        }
+    }
+
+}
