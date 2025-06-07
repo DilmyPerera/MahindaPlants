065a5f7a6e8257f56ba583fa6d4d70d250c5ec3a
DilmyPerera <samadiniperera20@gmail.com>
2025-06-07 17:30:34 +0530

    Add missing import for CategoryListPage styles

--- Frontend/react-ecommerce/src/component/pages/CategoryListPage.jsx
+++ Frontend/react-ecommerce/src/component/pages/CategoryListPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ApiService from "../../service/ApiService";
+import '../../style/CategoryListPage.css';
 
 const CategoryListPage = () => {
     const [categories, setCategories] = useState([]);
