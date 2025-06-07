c00c5832f7b45fd7cb179fd212b0418f853cb964
DilmyPerera <samadiniperera20@gmail.com>
2025-06-07 18:59:16 +0530

    Implement handleSubmit function for user login in LoginPage component

--- Frontend/react-ecommerce/src/component/pages/LoginPage.jsx
+++ Frontend/react-ecommerce/src/component/pages/LoginPage.jsx
@@ -16,4 +16,21 @@ const LoginPage = () => {
         const { name, value } = e.target;
         setFormData({ ...formData, [name]: value });
     }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await ApiService.loginUser(formData);
+            if (response.status === 200) {
+                setMessage("User Successfully Loged in");
+                localStorage.setItem('token', response.token);
+                localStorage.setItem('role', response.role);
+                setTimeout(() => {
+                    navigate("/profile")
+                }, 4000)
+            }
+        } catch (error) {
+            setMessage(error.response?.data.message || error.message || "unable to Login a user");
+        }
+    }
 }
