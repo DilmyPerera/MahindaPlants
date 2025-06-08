c4453d0fcc5356812d7741125b40efe8f3073e84
DilmyPerera <samadiniperera20@gmail.com>
2025-06-08 12:20:26 +0530

    Implement AddCategory component for category creation and navigation

--- Frontend/react-ecommerce/src/component/admin/AddCategory.jsx
+++ Frontend/react-ecommerce/src/component/admin/AddCategory.jsx
@@ -1,4 +1,28 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ApiService from "../../service/ApiService";
+
 const AddCategory = () => {
+    const [name, setName] = useState('');
+    const [message, setMessage] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await ApiService.createCategory({ name });
+            if (response.status === 200) {
+                setMessage(response.message);
+                setTimeout(() => {
+                    setMessage('');
+                    navigate("/admin/categories")
+                }, 3000)
+            }
+        } catch (error) {
+            setMessage(error.response?.data?.message || error.message || "Failed to save a category")
+        }
+    }
+
 
 }
 
