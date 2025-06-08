a46be3774f32b565f925edac6fe3a082070ab83a
DilmyPerera <samadiniperera20@gmail.com>
2025-06-08 15:09:37 +0530

    Add AdminOrdersPage and AdminOrderDetailsPage routes to App component

--- Frontend/react-ecommerce/src/App.js
+++ Frontend/react-ecommerce/src/App.js
@@ -20,6 +20,8 @@ import EditCategory from './component/admin/EditCategory';
 import AdminProductPage from './component/admin/AdminProductPage';
 import AddProductPage from './component/admin/AddProductPage';
 import EditProductPage from './component/admin/EditProductPage';
+import AdminOrdersPage from './component/admin/AdminOrdersPage';
+import AdminOrderDetailsPage from './component/admin/AdminOrderDetailsPage';
 
 function App() {
   return (
@@ -47,8 +49,8 @@ function App() {
           <Route path='/admin/add-product' element={<AdminRoute element={<AddProductPage />} />} />
           <Route path='/admin/edit-product/:productId' element={<AdminRoute element={<EditProductPage />} />} />
 
-
-
+          <Route path='/admin/orders' element={<AdminRoute element={<AdminOrdersPage />} />} />
+          <Route path='/admin/order-details/:itemId' element={<AdminRoute element={<AdminOrderDetailsPage />} />} />
 
 
         </Routes>
