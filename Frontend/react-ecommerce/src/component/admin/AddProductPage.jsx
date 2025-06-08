8df8e3634f94b1430f72110867b6607e1444a033
DilmyPerera <samadiniperera20@gmail.com>
2025-06-08 13:44:40 +0530

    Refactor handleSubmit function to improve product submission logic in AddProductPage

--- Frontend/react-ecommerce/src/component/admin/AddProductPage.jsx
+++ Frontend/react-ecommerce/src/component/admin/AddProductPage.jsx
@@ -20,6 +20,30 @@ const AddProductPage = () => {
     const handleImage = (e) => {
         setImage(e.target.files[0])
     }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const formData = new FormData();
+            formData.append('image', image);
+            formData.append('categoryId', categoryId);
+            formData.append('name', name);
+            formData.append('description', description);
+            formData.append('price', price);
+
+            const response = await ApiService.addProduct(formData);
+            if (response.status === 200) {
+                setMessage(response.message)
+                setTimeout(() => {
+                    setMessage('')
+                    navigate('/admin/products')
+                }, 3000);
+            }
+
+        } catch (error) {
+            setMessage(error.response?.data?.message || error.message || 'unable to upload product')
+        }
+    }
 }
 
 export default AddProductPage;
