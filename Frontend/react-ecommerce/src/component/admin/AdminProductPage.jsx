432a1524eb829f922d94a3212a2b5440ecd817ad
DilmyPerera <samadiniperera20@gmail.com>
2025-06-08 13:17:50 +0530

    Implement product fetching logic in AdminProductPage component

--- Frontend/react-ecommerce/src/component/admin/AdminProductPage.jsx
+++ Frontend/react-ecommerce/src/component/admin/AdminProductPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import ApiService from "../../service/ApiService";
 
 const AdminProductPage = () => {
     const navigate = useNavigate();
@@ -8,6 +9,18 @@ const AdminProductPage = () => {
     const [totalPages, setTotalPages] = useState(0);
     const [error, setError] = useState(null);
     const itemsPerPage = 10;
+
+    const fetchProducts = async () => {
+        try {
+            const response = await ApiService.getAllProducts();
+            const productList = response.productList || [];
+            setTotalPages(Math.ceil(productList.length / itemsPerPage));
+            setProducts(productList.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage));
+        } catch (error) {
+            setError(error.response?.data?.message || error.message || 'unable to fetch products')
+
+        }
+    }
 
 }
 
