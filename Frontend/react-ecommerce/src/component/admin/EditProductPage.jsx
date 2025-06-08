3d52343d7c016e93c87150ddc128e1e863107b9e
DilmyPerera <samadiniperera20@gmail.com>
2025-06-08 14:47:53 +0530

    Implement state management and routing for EditProductPage component

--- Frontend/react-ecommerce/src/component/admin/EditProductPage.jsx
+++ Frontend/react-ecommerce/src/component/admin/EditProductPage.jsx
@@ -1,5 +1,32 @@
-const EditProductPage = () => {
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import ApiService from "../../service/ApiService";
 
+const EditProductPage = () => {
+    const { productId } = useParams();
+    const [image, setImage] = useState(null);
+    const [categories, setCategories] = useState([]);
+    const [categoryId, setCategoryId] = useState('');
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [message, setMessage] = useState('');
+    const [price, setPrice] = useState('');
+    const [imageUrl, setImageUrl] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        ApiService.getAllCategory().then((res) => setCategories(res.categoryList));
+
+        if (productId) {
+            ApiService.getProductById(productId).then((response) => {
+                setName(response.product.name);
+                setDescription(response.product.description);
+                setPrice(response.product.price);
+                setCategoryId(response.product.categoryId);
+                setImageUrl(response.product.imageUrl);
+            })
+        }
+    }, [productId]);
 }
 
 export default EditProductPage;
