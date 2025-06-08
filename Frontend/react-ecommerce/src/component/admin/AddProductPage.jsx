66abdf946741ec1fa101a684df486fd89a658d41
DilmyPerera <samadiniperera20@gmail.com>
2025-06-08 13:43:32 +0530

    Add AddProductPage component for product addition functionality

---
+++ Frontend/react-ecommerce/src/component/admin/AddProductPage.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ApiService from "../../service/ApiService";
+
+const AddProductPage = () => {
+    const [image, setImage] = useState(null);
+    const [categories, setCategories] = useState([]);
+    const [categoryId, setCategoryId] = useState('');
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [message, setMessage] = useState('');
+    const [price, setPrice] = useState('');
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        ApiService.getAllCategory().then((res) => setCategories(res.categoryList));
+    }, [])
+}
+
+export default AddProductPage;
