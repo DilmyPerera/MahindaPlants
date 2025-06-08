3001093512203e65bfb1286d434d80ad9a80949b
DilmyPerera <samadiniperera20@gmail.com>
2025-06-08 13:05:21 +0530

    Initialize EditCategory component with state management and routing hooks

--- Frontend/react-ecommerce/src/component/admin/EditCategory.jsx
+++ Frontend/react-ecommerce/src/component/admin/EditCategory.jsx
@@ -1,5 +1,12 @@
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
 const EditCategory = () => {
 
+    const { categoryId } = useParams();
+    const [name, setName] = useState('')
+    const [message, setMessage] = useState('');
+    const navigate = useNavigate();
 }
 
 export default EditCategory;
