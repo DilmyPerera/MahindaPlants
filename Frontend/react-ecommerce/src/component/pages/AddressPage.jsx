115f82e8276dd9cf2b2fa6b9b52f0f1bc91498e4
DilmyPerera <samadiniperera20@gmail.com>
2025-06-08 08:28:20 +0530

    Initialize AddressPage component with address state

--- Frontend/react-ecommerce/src/component/pages/AddressPage.jsx
+++ Frontend/react-ecommerce/src/component/pages/AddressPage.jsx
@@ -1,3 +1,14 @@
+import { useState } from "react";
+
 const AddressPage = () => {
+
+    const [address, setAddress] = useState({
+        street: '',
+        city: '',
+        state: '',
+        zipCode: '',
+        country: ''
+    });
+
 
 }
